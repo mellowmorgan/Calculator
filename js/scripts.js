afbd389baafd1e6a74c5f5d2a09dc8b9cf18f2ed
rjashby <rjapsu160@gmail.com>
2021-10-18 13:06:15 -0700

    WIP - playing with calculaor functions.
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,8 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
   return number1 / number2;
 }
-
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 const result = subtract(number1, number2);
 alert(subtract(number1, number2));
+
