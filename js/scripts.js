0ab2813d66aed34a50179b2ca5989d331d902e90
mellowmorgan <morgan.waites@gmail.com>
2021-10-20 14:11:56 -0700

    Add user interface logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 function add(number1, number2) {
   return number1 + number2;
 }
+
 function subtract(number1, number2) {
   return number1 - number2;
 }
+
 function multiply(number1, number2) {
   return number1 * number2;
-}
+};
+
 function divide(number1, number2) {
   return number1 / number2;
-}
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = subtract(number1, number2);
-alert(subtract(number1, number2));
+};
 
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
